fbc9a4e59b0df663fe8bdf0c0f3f3c3b5b97f418
caro <carolina.benavides.venegas@ucreativa.com>
2021-05-27 21:28:25 -0600

    agregue vacunacion

--- src/com/ucreativa/Main.java
+++ src/com/ucreativa/Main.java
@@ -1,13 +1,13 @@
 package com.ucreativa;
 
 import com.ucreativa.familia.Carolina;
-import com.ucreativa.familia.Carolina;
+
 import com.ucreativa.familia.Maribel;
 
 public class Main {
 
     public static void main(String[] args) {
-        Carolina yo = new Carolina();
+        Carolina yo = new Carolina("hola soy caro");
         Maribel mama =new Maribel("hola chiquillos");
         mama.setcumple();
         int edad = mama.getEdad();
--- src/com/ucreativa/familia/Carolina.java
+++ src/com/ucreativa/familia/Carolina.java
@@ -1,8 +1,13 @@
 package com.ucreativa.familia;
 
-public class Carolina {
-    public Carolina(){
-        System.out.println("hola soy caro");
+public class Carolina extends Maribel {
+    private  String saludo;
+    private int edad;
+    private String hobby;
+
+
+    public Carolina(String saludo) {
+        super(saludo);
     }
 }
 
