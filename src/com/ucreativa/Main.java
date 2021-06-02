befee6861ffbf4941bf4b443fd5504625a9d3e24
caro <carolina.benavides.venegas@ucreativa.com>
2021-06-01 20:32:36 -0600

    agregue Bitacoravacunacion,amigo,familia

--- src/com/ucreativa/Main.java
+++ src/com/ucreativa/Main.java
@@ -1,12 +1,21 @@
 package com.ucreativa.vacunacion;
 
+import com.ucreativa.familia.Maribel;
+
+import java.util.ArrayList;
+import java.util.Date;
+
 public class Main {
 
     public static void main(String[] args) {
         Familia mama  = new Familia("Maribel","103450678",53,true,"mama");
-        Amigo carlos = new Amigo("jose","406870987",35,false,"amigo","N/A" );
+        Amigo jose = new Amigo("jose","406870987",35,false,"amigo","N/A" );
         Familia hermano = new Familia("Ricardo","402060476",34,true,"hermano");
 
+        ArrayList<BitacoraVacunas> bitacora = new ArrayList<>();
+        bitacora.add(new BitacoraVacunas(mama,"Pfizer", new Date()));
+        bitacora.add(new BitacoraVacunas(jose,"Astra",new Date()));
+        bitacora.add(new BitacoraVacunas(hermano,"Johnson",new Date()));
 
 
     }
