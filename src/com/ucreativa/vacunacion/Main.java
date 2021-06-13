c15f5e8625af00f73a8db9b70da1dad5d5c65312
caro <carolina.benavides.venegas@ucreativa.com>
2021-06-12 20:14:22 -0600

    trabajo en clase #9 cree FileRepository agregue condicion para salir del ciclo

--- src/com/ucreativa/vacunacion/Main.java
+++ src/com/ucreativa/vacunacion/Main.java
@@ -1,70 +1,63 @@
+package com.ucreativa.vacunacion;
+
 import com.ucreativa.vacunacion.entities.Amigo;
-import com.ucreativa.vacunacion.entities.BitacoraVacunas;
 import com.ucreativa.vacunacion.entities.Familia;
 import com.ucreativa.vacunacion.entities.Persona;
+import com.ucreativa.vacunacion.repositories.FileRepository;
 import com.ucreativa.vacunacion.repositories.InMemoryRepository;
+import com.ucreativa.vacunacion.repositories.Repository;
 
-import java.util.ArrayList;
+
 import java.util.Date;
-import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-    private static String print;
 
-    public static void main(String[] args)
         Scanner in = new Scanner(System.in);
-        List<BitacoraVacunas> db = new ArrayList<>();
-        InMemoryRepository repo = new InMemoryRepository();
+        Repository repo = new FileRepository();
+        String nombre, cedula, edad, riesgo, isAmigo, relacion, facebook, parentesco, marca, print;
         Persona persona;
-        String nombre, cedula, edad, riesgo, isAmigo, relacion, facebook, parentesco, marca;
-
-        while (true) {
-            String nombre, cedula, edad, riesgo, isAmigo, relacion, facebook, parentesco, marca;
-            Persona persona;
+        boolean continuar = true;
+        while (continuar) {
             System.out.println("Nombre");
             nombre = in.nextLine();
             System.out.println("Cedula");
-
-        }
-        System.out.println("Nombre:");
-            nombre = in.nextLine();
-            System.out.println("Cedula:");
             cedula = in.nextLine();
-            System.out.println("Edad:");
+            System.out.println("Edad");
             edad = in.nextLine();
-            System.out.println("Riesgo(S/N):");
+            System.out.println("Riesgo");
             riesgo = in.nextLine();
-            System.out.println("Amigo(A)/Familia(F):");
+            System.out.println("Amigo(A)/Familiar(F):");
             isAmigo = in.nextLine();
             if (isAmigo.equals("A")){
                 System.out.println(("Relacion:"));
                 relacion = in.nextLine();
                 System.out.println("Facebook:");
                 facebook = in.nextLine();
-                persona = new Amigo(nombre, cedula, Integer.parseInt (edad), riesgo.equals("S"), relacion, facebook);
+                persona = new Amigo(nombre, cedula, Integer.parseInt(edad), riesgo.equals("S"), relacion, facebook);
 
             } else {
                 System.out.println("Parentesco:");
                 parentesco = in.nextLine();
-                persona = new Familia (nombre, cedula,Integer.parseInt (edad), riesgo.equals("S"), parentesco );
+                persona = new Familia(nombre, cedula,Integer.parseInt (edad), riesgo.equals("S"), parentesco );
 
             }
             System.out.println("Vacuna--Marca:");
             marca = in.nextLine();
-            repo.save(persona,marca,new Date()););
+            repo.save(persona,marca,new Date());
             System.out.println("Quiere imprimir Lista (S)");
-            String print = in.nextLine();
+            print = in.nextLine();
+
             if (print.equals("S")){
                 for (String item : repo.get()) {
                     System.out.println(item);
                 }
-
-           }
-
+            }
+            System.out.println("Desea continuar(S)");
+            String txtContinuar = in.nextLine();
+            continuar = !txtContinuar.equals("N");
         }
     }
-
 }
--- src/com/ucreativa/vacunacion/repositories/InMemoryRepository.java
+++ src/com/ucreativa/vacunacion/repositories/InMemoryRepository.java
@@ -1,37 +1,37 @@
 package com.ucreativa.vacunacion.repositories;
 
+
+
 import com.ucreativa.vacunacion.entities.BitacoraVacunas;
 import com.ucreativa.vacunacion.entities.Persona;
-import jdk.jfr.Percentage;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-public class InMemoryRepository {
+public class InMemoryRepository implements Repository{
 
-    private List<BitacoraVacunas> db;
+    private final List<BitacoraVacunas> db;
     public InMemoryRepository(){
+
         this.db = new ArrayList<>();
     }
     public void save(Persona persona, String marca, Date fecha){
-        this.db.add(new BitacoraVacunas(persona, marca, fecha));
+        this.db.add(new BitacoraVacunas (persona, marca, fecha));
     }
 
-    public void get() {
+    public List<String> get() {
         List<String> lines = new ArrayList<>();
-        String pattern;
-        SimpleDateFormat format = new SimpleDateFormat("dd-MM-yyyy hh:mm:ss");
-        for (BitacoraVacunas item : db){
+        SimpleDateFormat format = new SimpleDateFormat( "dd-MM-yyyy hh:mm:ss");
+        for (BitacoraVacunas item: db){
             lines.add(item.getPersona().getNombre()
-                    +"-"+ item.getMarca()
-                    +"+"+ format.format(item.getFecha());
+                    + " - " + item.getMarca()
+                    + " + " + format.format(item.getFecha()));
         }
         return lines;
     }
 }
-
 
 
 
